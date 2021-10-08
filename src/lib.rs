e09e10f26847c2a13e27fbe2e28271b9e9525818
fzyzcjy <ch271828n@outlook.com>
2021-10-08 20:21:14 +0800

    add an exaple from https://docs.rs/syn/1.0.80/syn/fold/index.html#example

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,29 @@
 use proc_macro::TokenStream;
 
+use quote::quote;
 use syn::*;
+use syn::fold::{Fold, fold_expr};
 
 #[proc_macro]
 pub fn opencv_expr(input: TokenStream) -> TokenStream {
-    let tmp = input.clone();
-    let ast: Expr = parse_macro_input!(input);
+    let src_ast: Expr = parse_macro_input!(input);
+    println!("opencv_expr src_ast={:#?}", src_ast);
 
-    println!("ast={:?}", ast);
+    let ans_ast = OpencvExpr.fold_expr(src_ast);
+    println!("opencv_expr ans_ast={:#?}", ans_ast);
 
-    tmp
+    quote!(#ans_ast).into()
+}
+
+struct OpencvExpr;
+
+impl Fold for OpencvExpr {
+    // ref: https://docs.rs/syn/1.0.80/syn/fold/index.html#example
+    fn fold_expr(&mut self, expr: Expr) -> Expr {
+        Expr::Paren(ExprParen {
+            attrs: Vec::new(),
+            expr: Box::new(fold_expr(self, expr)),
+            paren_token: token::Paren::default(),
+        })
+    }
 }
