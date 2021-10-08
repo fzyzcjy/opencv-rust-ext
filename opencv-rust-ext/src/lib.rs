99411d34a9fe1c181c7a9de3acfc9a0d50aeb424
fzyzcjy <ch271828n@outlook.com>
2021-10-08 20:28:39 +0800

    add back from_point and mat_view

--- opencv-rust-ext/src/lib.rs
+++ opencv-rust-ext/src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub use expr_macro::*;
+pub use mat_view::*;
+pub use misc::*;
+pub use opencv_macro::*;
+
+mod mat_view;
+mod misc;
