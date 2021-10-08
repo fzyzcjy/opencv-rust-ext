dd37245a94cc0463bf0fbb3453e62175b0be0ec9
fzyzcjy <ch271828n@outlook.com>
2021-10-08 21:19:14 +0800

    minor change on testing example

--- opencv-rust-ext/tests/expr_macro.rs
+++ opencv-rust-ext/tests/expr_macro.rs
@@ -5,13 +5,13 @@ use opencv_ext::IntoResult;
 
 #[test]
 fn test_expr_macro() -> Result<()> {
-    let a = 42i32;
-    let b = 100i32;
-    let c = 50i32;
+    let a = 42;
+    let b = 100;
+    let c = 50;
 
-    // let result = failable_expr! { a - b / c };
+    let result = failable_expr! { a - b / c };
 
-    let result = failable_expr! { a - b };
+    // let result = failable_expr! { a - b };
     // let result = ((((a.into_result())?) - ((b.into_result())?).into_result())?);
 
     // let result = failable_expr! { a };
