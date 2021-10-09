6d79aa91007e985f2a78a5cd4dfa98c49091ddae
fzyzcjy <ch271828n@outlook.com>
2021-10-09 20:09:25 +0800

    tests

--- opencv-rust-ext/src/into_result.rs
+++ opencv-rust-ext/src/into_result.rs
@@ -1,41 +1,41 @@
-use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
-
-pub trait IntoResult<T, E> {
-    fn into_result(self) -> Result<T, E>;
-}
-
-impl<T, E> IntoResult<T, E> for Result<T, E> {
-    fn into_result(self) -> Result<T, E> {
-        self
-    }
-}
-
-impl<T> IntoResult<T, IntoResultDummyError> for T {
-    fn into_result(self) -> Result<T, IntoResultDummyError> {
-        Ok(self)
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct IntoResultDummyError;
-
-impl Display for IntoResultDummyError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "IntoResultDummyError")
-    }
-}
-
-impl Error for IntoResultDummyError {}
-
-#[cfg(test)]
-mod test {
-    use crate::IntoResult;
-
-    #[test]
-    fn test_into_result() {
-        assert_eq!(Ok(42), 42.into_result());
-        assert_eq!(Result::<i32, _>::Err("oops"), Err("oops").into_result());
-        assert_eq!(Result::<_, &str>::Ok(100), Ok(100).into_result());
-    }
-}
+// use std::error::Error;
+// use std::fmt::{Debug, Display, Formatter};
+//
+// pub trait IntoResult<T, E> {
+//     fn into_result(self) -> Result<T, E>;
+// }
+//
+// impl<T, E> IntoResult<T, E> for Result<T, E> {
+//     fn into_result(self) -> Result<T, E> {
+//         self
+//     }
+// }
+//
+// impl<T> IntoResult<T, IntoResultDummyError> for T {
+//     fn into_result(self) -> Result<T, IntoResultDummyError> {
+//         Ok(self)
+//     }
+// }
+//
+// #[derive(Debug, Clone, Eq, PartialEq)]
+// pub struct IntoResultDummyError;
+//
+// impl Display for IntoResultDummyError {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "IntoResultDummyError")
+//     }
+// }
+//
+// impl Error for IntoResultDummyError {}
+//
+// #[cfg(test)]
+// mod test {
+//     use crate::IntoResult;
+//
+//     #[test]
+//     fn test_into_result() {
+//         assert_eq!(Ok(42), 42.into_result());
+//         assert_eq!(Result::<i32, _>::Err("oops"), Err("oops").into_result());
+//         assert_eq!(Result::<_, &str>::Ok(100), Ok(100).into_result());
+//     }
+// }
--- opencv-rust-ext/tests/expr_macro.rs
+++ opencv-rust-ext/tests/expr_macro.rs
@@ -1,28 +1,54 @@
 use anyhow::Result;
-use opencv::core::CV_8UC1;
+
+use opencv::core::*;
 use opencv::prelude::*;
 
-use opencv_ext::failable_expr;
-use opencv_ext::IntoResult;
-
-// does not compile yet
-// #[test]
-// fn test_expr_macro_opencv() -> Result<()> {
-//     let a = Mat::new_rows_cols_with_default(3, 5, CV_8UC1, 42.0.into())?;
-//     let b = Mat::new_rows_cols_with_default(3, 5, CV_8UC1, 100.0.into())?;
-//     let c = 10u8;
-//     assert_eq!(
-//         failable_expr! { a + b - c },
-//         Mat::new_rows_cols_with_default(3, 5, CV_8UC1, 132.0.into())
-//     );
-//     Ok(())
-// }
+// use opencv_ext::MyIntoResult;
 
 #[test]
-fn test_expr_macro_simple() -> Result<()> {
-    let a = 42;
-    let b = 100;
-    let c = 10u8;
-    assert_eq!(failable_expr! { a + b - c }, 132,);
+fn test_expr_macro_opencv() -> Result<()> {
+    let a = Mat::new_rows_cols_with_default(2, 3, CV_8UC1, 100.0.into())?;
+    let b = Mat::new_rows_cols_with_default(2, 3, CV_8UC1, 10.0.into())?;
+    let c = Mat::new_rows_cols_with_default(2, 3, CV_8UC1, 50.0.into())?;
+    let d = Mat::new_rows_cols_with_default(2, 3, CV_8UC1, 2.0.into())?;
+    // let c: Mat = (failable_expr! { a - b }).to_mat()?;
+    // let c: Mat = ((((a).into_result()?) - ((b).into_result()?)).into_result()?).to_mat()?;
+    // let c: Mat = (a.into_result()? - b.into_result()?).into_result()?.to_mat()?;
+
+    // ok
+    // let a_sub_b: MatExpr = (a - b).my_into_result()?;
+    // let a_sub_b_sub_c: MatExpr = (a_sub_b - c).my_into_result()?;
+    // let a_sub_b_sub_c_mat: Mat = a_sub_b_sub_c.to_mat()?;
+
+    // not ok
+    // let a_sub_b = (a - b).my_into_result()?;
+    // let a_sub_b_sub_c: MatExpr = (a_sub_b - c).my_into_result()?;
+
+    // ok
+    // let a_sub_b = (a - b)?;
+    // let a_sub_b_sub_c: MatExpr = (a_sub_b - c)?;
+
+    // let a_sub_b = a - b;
+    // let a_sub_b_sub_c = a_sub_b - c;
+    // let a_sub_b_sub_c_ans = a_sub_b_sub_c.into_result()?;
+
+    // let u = &a - &b;
+    // let x = ((&a - &b) - (&c - &d)).into_result()?.to_mat()?;
+    let x = (&a / d - &b + &c - 10f64).into_result()?.to_mat()?;
+    println!("x={:?}", x.data_bytes());
+
+    // assert_eq!(
+    //     c.data_bytes()?,
+    //     Mat::new_rows_cols_with_default(3, 5, CV_8UC1, 80.0.into())?.data_bytes()?,
+    // );
     Ok(())
 }
+
+// #[test]
+// fn test_expr_macro_simple() -> Result<()> {
+//     let a = 42;
+//     let b = 100;
+//     let c = 10u8;
+//     assert_eq!(failable_expr! { a + b - c }, 132,);
+//     Ok(())
+// }
